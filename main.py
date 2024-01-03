d4cee8f9f621488846936e6ff2cbec6f35b32b2a
Joel Fu <joelbfu@gmail.com>
2024-01-03 01:25:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ root.resizable(False, False)
 root['bg'] = '#000000'
 
 #Add text
-text = tk.Label(root, height=2, width=70, bg='#000000', fg='#FFFFFF', bd=0, text='Enter Youtube URL:', font=('Ariel',20))
+text = tk.Label(root, height=3, width=70, bg='#000000', fg='#FFFFFF', bd=0, text='Enter Youtube URL:', font=('Ariel',20))
 text.pack(pady=20)
 
 #Get youtube link
 youtubelink = tk.Text(root, height=2, width=70)
-youtubelink.pack(pady=30)
+youtubelink.pack(pady=20)
 
 link = extractor(youtubelink)
 
 download_img = tk.PhotoImage(file='imgs/download.png')
 download_button = tk.Button(root,image=download_img,command=link.download_file,border='2')
-download_button.pack(pady=40)
+download_button.pack(pady=30)
 
 root.mainloop() 
